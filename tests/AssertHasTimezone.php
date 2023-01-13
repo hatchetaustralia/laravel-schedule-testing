b841723a0152c7b85afe0237953716e539498d7e
Glenn Forrest <glennjamesforrest@gmail.com>
2023-01-13 19:46:09 +0800

    test: add missing failure assertion for hasTimezone - closes #1

--- tests/AssertHasTimezone.php
+++ tests/AssertHasTimezone.php
@@ -4,6 +4,7 @@ namespace Tests;
 
 use DateTimeZone;
 use Tests\TestCase;
+use PHPUnit\Framework\AssertionFailedError;
 use Hatchet\LaravelScheduleTesting\InteractsWithSchedule;
 
 final class AssertHasTimezone extends TestCase
@@ -34,5 +35,22 @@ final class AssertHasTimezone extends TestCase
 
         $this->assertSchedule('fake:command')
             ->hasTimezone('Australia/Perth');
+    }
+
+    public function testHasTimezoneFailure(): void
+    {
+        $this->fakeScheduledCommand();
+
+        $this->expectException(AssertionFailedError::class);
+
+        $this->expectExceptionMessage(
+            "Command [fake:command] does not have the timezone Australia/Perth."
+        );
+
+        $this->assertSchedule('fake:command')
+            ->hasTimezone('Australia/Perth');
+
+        $this->assertSchedule('fake:command')
+            ->hasTimezone(new DateTimeZone('Australia/Perth'));
     }
 }
