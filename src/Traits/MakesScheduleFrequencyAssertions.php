3d11c609588c42a9eb5810a36d652da1226db3d6
Glenn Forrest <glennjamesforrest@gmail.com>
2023-02-01 22:06:40 +0800

    wip: schedule frequency assertions #13

--- src/Traits/MakesScheduleFrequencyAssertions.php
+++ src/Traits/MakesScheduleFrequencyAssertions.php
@@ -2,7 +2,10 @@
 
 namespace Hatchet\LaravelScheduleTesting\Traits;
 
+use NumberFormatter;
+use Illuminate\Support\Arr;
 use PHPUnit\Framework\Assert;
+use Illuminate\Console\Scheduling\Event;
 use Illuminate\Console\Scheduling\ManagesFrequencies;
 
 trait MakesScheduleFrequencyAssertions
@@ -11,14 +14,211 @@ trait MakesScheduleFrequencyAssertions
 
     public string $expression = '* * * * *';
 
-    public function runsEveryMinute(): self
+    private function assertScheduleFrequency(string $message): self
     {
-        Assert::assertEquals(
-            $cronExpression = '* * * * *',
-            $this->everyMinute()->expression,
-            $this->failureMessage("does not run every minute. Cron expression does not match {$cronExpression}.")
+        Assert::assertTrue(
+            $this->scheduledEvents->some(
+                fn (Event $event): bool => $event->expression == $this->expression,
+            ),
+            $this->failureMessage("does not run {$message}. Cron expression does not match {$this->expression}.")
         );
 
         return $this;
+    }
+
+    public function runsEveryMinute(): self
+    {
+        return $this->everyMinute()
+            ->assertScheduleFrequency('every minute');
+    }
+
+    public function runsEveryTwoMinutes(): self
+    {
+        return $this->everyTwoMinutes()
+            ->assertScheduleFrequency('every 2 minutes');
+    }
+
+    public function runsEveryThreeMinutes(): self
+    {
+        return $this->everyThreeMinutes()
+            ->assertScheduleFrequency('every 3 minutes');
+    }
+
+    public function runsEveryFourMinutes(): self
+    {
+        return $this->everyFourMinutes()
+            ->assertScheduleFrequency('every four minutes');
+    }
+
+    public function runsEveryFiveMinutes(): self
+    {
+        return $this->everyFiveMinutes()
+            ->assertScheduleFrequency('every five minutes');
+    }
+
+    public function runsEveryTenMinutes(): self
+    {
+        return $this->everyTenMinutes()
+            ->assertScheduleFrequency('every 10 minutes');
+    }
+
+    public function runsEveryFifteenMinutes(): self
+    {
+        return $this->everyFifteenMinutes()
+            ->assertScheduleFrequency('every 15 minutes');
+    }
+
+    public function runsEveryThirtyMinutes(): self
+    {
+        return $this->everyThirtyMinutes()
+            ->assertScheduleFrequency('every 30 minutes');
+    }
+
+    public function runsHourly(): self
+    {
+        return $this->hourly()
+            ->assertScheduleFrequency('every hour');
+    }
+
+    public function runsEveryOddHour(): self
+    {
+        return $this->everyOddHour()
+            ->assertScheduleFrequency('every odd hour');
+    }
+
+    public function runsEveryTwoHours(): self
+    {
+        return $this->everyTwoHours()
+            ->assertScheduleFrequency('every 2 hours');
+    }
+
+    public function runsEveryThreeHours(): self
+    {
+        return $this->everyThreeHours()
+            ->assertScheduleFrequency('every 3 hours');
+    }
+
+    public function runsEveryFourHours(): self
+    {
+        return $this->everyFourHours()
+            ->assertScheduleFrequency('every 4 hours');
+    }
+
+    public function runsEverySixHours(): self
+    {
+        return $this->everySixHours()
+            ->assertScheduleFrequency('every 6 hours');
+    }
+
+    public function runsDaily(): self
+    {
+        return $this->daily()
+            ->assertScheduleFrequency('daily');
+    }
+
+    public function runsTwiceDaily($first = 1, $second = 13): self
+    {
+        $firstHour = $this->addLeadingZero($first);
+        $secondHour = $this->addLeadingZero($second);
+
+        return $this->twiceDaily($first, $second)
+            ->assertScheduleFrequency("twice daily at {$firstHour}:00 and {$secondHour}:00");
+    }
+
+    public function runsWeekly(): self
+    {
+        return $this->weekly()
+            ->assertScheduleFrequency('weekly');
+    }
+
+    public function runsMonthly(): self
+    {
+        return $this->monthly()
+            ->assertScheduleFrequency('monthly');
+    }
+
+    public function runsTwiceMonthly(): self
+    {
+        return $this->twiceMonthly()
+            ->assertScheduleFrequency('twice monthly');
+    }
+
+    public function runsQuarterly(): self
+    {
+        return $this->quarterly()
+            ->assertScheduleFrequency('quarterly');
+    }
+
+    public function runsYearly(): self
+    {
+        return $this->yearly()
+            ->assertScheduleFrequency('yearly');
+    }
+
+    public function runsHourlyAt(array|int $offset): self
+    {
+        $minutes = collect(Arr::wrap($offset));
+
+        return $this->hourlyAt($offset)
+            ->assertScheduleFrequency("hourly at {$minutes->implode(' and ')}");
+    }
+
+    public function runsDailyAt(string|int $time): self
+    {
+        return $this->dailyAt($time)
+            ->assertScheduleFrequency("daily at {$time}");
+    }
+
+    public function runsTwiceDailyAt($first = 1, $second = 13, $offset = 0): self
+    {
+        $minutes = $this->addLeadingZero($offset);
+
+        return $this->twiceDailyAt($first, $second, $offset)
+            ->assertScheduleFrequency("twice daily at {$first}:{$minutes} and {$second}:{$minutes}");
+    }
+
+    public function runsWeeklyOn(array|string|int $dayOfWeek, $time = '0:0'): self
+    {
+        $formatter = new NumberFormatter('en_AU', NumberFormatter::ORDINAL);
+
+        $days = collect(Arr::wrap($dayOfWeek))
+            ->map(fn ($day) => $formatter->format($day));
+
+        $times = explode(':', $time);
+
+        $hour = $this->addLeadingZero($times[0]);
+        $minutes = $this->addLeadingZero(isset($times[1]) ? $times[1] : 0);
+
+        return $this->weeklyOn($dayOfWeek, $time)
+            ->assertScheduleFrequency("on the {$days->implode(' and ' )} day of the week at {$hour}:{$minutes}");
+    }
+
+    public function runsMonthlyOn(): self
+    {
+        return $this->monthlyOn()
+            ->assertScheduleFrequency('');
+    }
+
+    public function runsLastDayOfMonth(): self
+    {
+        return $this->lastDayOfMonth()
+            ->assertScheduleFrequency('');
+    }
+
+    public function runsQuarterlyOn(): self
+    {
+        return $this->quarterlyOn()
+            ->assertScheduleFrequency('');
+    }
+
+    public function runsYearlyOn(): self
+    {
+        return $this->yearlyOn()
+            ->assertScheduleFrequency('');
+    }
+
+    private function addLeadingZero(int|string $number): string
+    {
+        return str_pad($number, 2, '0', STR_PAD_LEFT);
     }
 }
--- tests/AssertScheduleFrequency.php
+++ tests/AssertScheduleFrequency.php
@@ -20,55 +20,205 @@ final class AssertScheduleFrequency extends TestCase
         $this->assertSchedule('fake:command')->{$assertion}();
     }
 
-    public function testScheduleFrequencyFailure(): void
+    /**
+     * @dataProvider mismatchedScheduleFrequencies
+     */
+    public function testScheduleFrequencyFailure(string $scheduleFrequency, string $assertion): void
     {
-        // $this->fakeScheduledCommand();
+        $this->fakeScheduledCommand()->{$scheduleFrequency}();
 
-        // $this->expectException(AssertionFailedError::class);
+        $this->expectException(AssertionFailedError::class);
+        $this->expectExceptionMessageMatches('/Command \[fake:command\] does not run/');
 
-        // $this->expectExceptionMessage(
-        //     "Command [fake:command] does not have the timezone Australia/Perth."
-        // );
-
-        // $this->assertSchedule('fake:command')
-        //     ->hasTimezone('Australia/Perth');
-
-        // $this->assertSchedule('fake:command')
-        //     ->hasTimezone(new DateTimeZone('Australia/Perth'));
+        $this->assertSchedule('fake:command')->{$assertion}();
     }
+
+    public function testHourlyAt(): void
+    {
+        // single time
+        $this->fakeScheduledCommand()->hourlyAt(20);
+
+        $this->assertSchedule('fake:command')
+            ->runsHourlyAt(20);
+
+        // multiple times
+        $this->fakeScheduledCommand()->hourlyAt([20, 30]);
+
+        $this->assertSchedule('fake:command')
+            ->runsHourlyAt([20, 30]);
+    }
+
+    public function testHourlyAtFailure(): void
+    {
+        $this->fakeScheduledCommand()->hourlyAt(20);
+
+        $this->expectException(AssertionFailedError::class);
+        $this->expectExceptionMessageMatches('/Command \[fake:command\] does not run hourly at 10/');
+
+        $this->assertSchedule('fake:command')
+            ->runsHourlyAt(10);
+
+        $this->fakeScheduledCommand()->hourlyAt([15, 20]);
+
+        $this->expectException(AssertionFailedError::class);
+        $this->expectExceptionMessageMatches('/Command \[fake:command\] does not run hourly at 1 and 2/');
+
+        $this->assertSchedule('fake:command')
+            ->runsHourlyAt([1, 2]);
+    }
+
+    public function testDailyAt(): void
+    {
+        $this->fakeScheduledCommand()->dailyAt('16:30');
+
+        $this->assertSchedule('fake:command')
+            ->runsDailyAt('16:30');
+    }
+
+    public function testDailyAtFailure(): void
+    {
+        $this->fakeScheduledCommand()->dailyAt(20);
+
+        $this->expectException(AssertionFailedError::class);
+        $this->expectExceptionMessageMatches('/Command \[fake:command\] does not run daily at 16:00/');
+
+        $this->assertSchedule('fake:command')
+            ->runsDailyAt('16:00');
+    }
+
+    public function testTwiceDailyWithArguments(): void
+    {
+        $this->fakeScheduledCommand()->twiceDaily('20', '30');
+
+        $this->assertSchedule('fake:command')
+            ->runsTwiceDaily('20', '30');
+    }
+
+    public function testTwiceDailyWithArgumentsFailure(): void
+    {
+        $this->fakeScheduledCommand()->twiceDaily();
+
+        $this->expectException(AssertionFailedError::class);
+        $this->expectExceptionMessageMatches('/Command \[fake:command\] does not run twice daily at 12:00 and 16:00/');
+
+        $this->assertSchedule('fake:command')
+            ->runsTwiceDaily(12, 16);
+    }
+
+    public function testTwiceDailyAt(): void
+    {
+        $this->fakeScheduledCommand()->twiceDailyAt(8, 10, 6);
+
+        $this->assertSchedule('fake:command')
+            ->runsTwiceDailyAt(8, 10, 6);
+    }
+
+    public function testTwiceDailyAtFailure(): void
+    {
+        $this->fakeScheduledCommand()->twiceDailyAt();
+
+        $this->expectException(AssertionFailedError::class);
+        $this->expectExceptionMessageMatches('/Command \[fake:command\] does not run twice daily at 12:15 and 16:15/');
+
+        $this->assertSchedule('fake:command')
+            ->runsTwiceDailyAt(12, 16, 15);
+    }
+
+    public function testWeeklyOn(): void
+    {
+        $this->fakeScheduledCommand()->weeklyOn(5);
+
+        $this->assertSchedule('fake:command')
+            ->runsWeeklyOn(5);
+
+        $this->fakeScheduledCommand()->weeklyOn([1, 5]);
+
+        $this->assertSchedule('fake:command')
+            ->runsWeeklyOn([1, 5]);
+
+        $this->fakeScheduledCommand()->weeklyOn([1, 5], '23:23');
+
+        $this->assertSchedule('fake:command')
+            ->runsWeeklyOn([1, 5], '23:23');
+    }
+
+    public function testWeeklyOnFailure(): void
+    {
+        $this->fakeScheduledCommand()->weeklyOn(0);
+
+        $this->expectException(AssertionFailedError::class);
+        $this->expectExceptionMessageMatches('/Command \[fake:command\] does not run on the 5th day of the week at 15:00/');
+
+        $this->assertSchedule('fake:command')
+            ->runsWeeklyOn(5, 15);
+
+        $this->expectException(AssertionFailedError::class);
+        $this->expectExceptionMessageMatches('/Command \[fake:command\] does not run on the 3rd and 5th day of the week at 12:35/');
+
+        $this->assertSchedule('fake:command')
+            ->runsWeeklyOn([3, 5], '12:35');
+    }
+
+    // todo - finish these assertions off
+    // ['monthlyOn', 'runsMonthlyOn'],
+    // ['lastDayOfMonth', 'runsLastDayOfMonth'],
+    // ['quarterlyOn', 'runsQuarterlyOn'],
+    // ['yearlyOn', 'runsYearlyOn'],
 
     public function scheduleFrequencies(): array
     {
         return [
             ['everyMinute', 'runsEveryMinute'],
-            // ['everyTwoMinutes', 'runsEveryTwoMinutes'],
-            // ['everyThreeMinutes', 'runsEveryThreeMinutes'],
-            // ['everyFourMinutes', 'runsEveryFourMinutes'],
-            // ['everyFiveMinutes', 'runsEveryFiveMinutes'],
-            // ['everyTenMinutes', 'runsEveryTenMinutes'],
-            // ['everyFifteenMinutes', 'runsEveryFifteenMinutes'],
-            // ['everyThirtyMinutes', 'runsEveryThirtyMinutes'],
-            // ['hourly', 'runsHourly'],
-            // ['hourlyAt', 'runsHourlyAt'],
-            // ['everyOddHour', 'runsEveryOddHour'],
-            // ['everyTwoHours', 'runsEveryTwoHours'],
-            // ['everyThreeHours', 'runsEveryThreeHours'],
-            // ['everyFourHours', 'runsEveryFourHours'],
-            // ['everySixHours', 'runsEverySixHours'],
-            // ['daily', 'runsDaily'],
-            // ['dailyAt', 'runsDailyAt'],
-            // ['twiceDaily', 'runsTwiceDaily'],
-            // ['twiceDailyAt', 'runsTwiceDailyAt'],
-            // ['weekly', 'runsWeekly'],
-            // ['weeklyOn', 'runsWeeklyOn'],
-            // ['monthly', 'runsMonthly'],
-            // ['monthlyOn', 'runsMonthlyOn'],
-            // ['twiceMonthly', 'runsTwiceMonthly'],
-            // ['lastDayOfMonth', 'runsLastDayOfMonth'],
-            // ['quarterly', 'runsQuarterly'],
-            // ['quarterlyOn', 'runsQuarterlyOn'],
-            // ['yearly', 'runsYearly'],
-            // ['yearlyOn', 'runsYearlyOn'],
+            ['everyTwoMinutes', 'runsEveryTwoMinutes'],
+            ['everyThreeMinutes', 'runsEveryThreeMinutes'],
+            ['everyFourMinutes', 'runsEveryFourMinutes'],
+            ['everyFiveMinutes', 'runsEveryFiveMinutes'],
+            ['everyTenMinutes', 'runsEveryTenMinutes'],
+            ['everyFifteenMinutes', 'runsEveryFifteenMinutes'],
+            ['everyThirtyMinutes', 'runsEveryThirtyMinutes'],
+            ['hourly', 'runsHourly'],
+
+            ['everyOddHour', 'runsEveryOddHour'],
+            ['everyTwoHours', 'runsEveryTwoHours'],
+            ['everyThreeHours', 'runsEveryThreeHours'],
+            ['everyFourHours', 'runsEveryFourHours'],
+            ['everySixHours', 'runsEverySixHours'],
+            ['daily', 'runsDaily'],
+
+            ['twiceDaily', 'runsTwiceDaily'],
+            ['weekly', 'runsWeekly'],
+            ['monthly', 'runsMonthly'],
+            ['twiceMonthly', 'runsTwiceMonthly'],
+            ['quarterly', 'runsQuarterly'],
+            ['yearly', 'runsYearly'],
+        ];
+    }
+
+    public function mismatchedScheduleFrequencies(): array
+    {
+        return [
+            ['everyMinute', 'runsHourly'],
+            ['everyTwoMinutes', 'runsHourly'],
+            ['everyThreeMinutes', 'runsHourly'],
+            ['everyFourMinutes', 'runsHourly'],
+            ['everyFiveMinutes', 'runsHourly'],
+            ['everyTenMinutes', 'runsHourly'],
+            ['everyFifteenMinutes', 'runsHourly'],
+            ['everyThirtyMinutes', 'runsHourly'],
+            ['hourly', 'runsEveryMinute'],
+            ['everyOddHour', 'runsHourly'],
+            ['everyTwoHours', 'runsHourly'],
+            ['everyThreeHours', 'runsHourly'],
+            ['everyFourHours', 'runsHourly'],
+            ['everySixHours', 'runsHourly'],
+            ['daily', 'runsHourly'],
+            ['twiceDaily', 'runsHourly'],
+            ['weekly', 'runsHourly'],
+            ['monthly', 'runsHourly'],
+            ['twiceMonthly', 'runsHourly'],
+            ['lastDayOfMonth', 'runsHourly'],
+            ['quarterly', 'runsHourly'],
+            ['yearly', 'runsHourly'],
         ];
     }
 }
